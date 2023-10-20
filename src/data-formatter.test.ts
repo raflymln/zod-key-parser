19d51ea8578fe845318bf242ce14fc8467053eda
Rafly Maulana <mraflymaulana@gmail.com>
2023-10-20 23:57:57 +0700

    chore: move test file

--- src/data-formatter.test.ts
+++ src/data-formatter.test.ts
@@ -7,115 +7,6 @@ import { File } from "@web-std/file";
 import assert from "assert";
 import { randomUUID } from "crypto";
 
-const testFile = new File(["test"], "test.ts");
-
-// const testZodSchemaParser = () => {
-//     const schema = z.object({
-//         key1: z.boolean(),
-//         key2: z.string().nullable(),
-//         key3: z.enum(["a", "b", "c"]),
-//         array: z.array(
-//             z.object({
-//                 key1: z.number(),
-//                 key2: z.string(),
-//                 key3: z.boolean(),
-//                 key4: z.string(),
-//             })
-//         ),
-//         object: z
-//             .object({
-//                 ia: z.number(),
-//             })
-//             .optional(),
-//         union: z.union([z.string(), z.number()]),
-//         unionObject: z.union([
-//             z.object({
-//                 a: z.string(),
-//             }),
-//             z.object({
-//                 b: z.number(),
-//             }),
-//         ]),
-//         intersection: z.intersection(
-//             z.object({
-//                 a: z.string(),
-//             }),
-//             z.object({
-//                 b: z.number(),
-//             })
-//         ),
-//     });
-
-//     console.log("> Begin test to parse Zod Schema");
-//     const result = parseZodSchema(schema);
-
-//     // #region Keys
-//     console.log("- Testing Keys");
-//     console.log(result.keys);
-
-//     if (result.keys.key1 !== "key1" || result.keys.key2 !== "key2") {
-//         throw new Error("Failed to parse keys");
-//     }
-
-//     if (typeof result.keys.array !== "function" || result.keys.array.key !== "array") {
-//         throw new Error("Failed to parse keys on array");
-//     }
-
-//     if (result.keys.array(0).key1 !== "array.0.key1") {
-//         throw new Error("Failed to parse keys on array value");
-//     }
-
-//     if (typeof result.keys.object !== "object" || result.keys.object.ia !== "object.ia") {
-//         throw new Error("Failed to parse keys on object");
-//     }
-
-//     if (result.keys.union !== "union") {
-//         throw new Error("Failed to parse keys on union");
-//     }
-
-//     if (result.keys.unionObject.a !== "unionObject.a" || result.keys.unionObject.b !== "unionObject.b") {
-//         throw new Error("Failed to parse keys on union object");
-//     }
-
-//     if (result.keys.intersection.a !== "intersection.a" || result.keys.intersection.b !== "intersection.b") {
-//         throw new Error("Failed to parse keys on intersection");
-//     }
-
-//     // #endregion
-
-//     // #region Prisma Keys
-//     console.log("- Testing Prisma Keys");
-//     console.log(result.prismaKeys);
-
-//     if (result.prismaKeys.key1 !== true || result.prismaKeys.key2 !== true) {
-//         throw new Error("Failed to parse prisma keys");
-//     }
-
-//     if (!result.prismaKeys.array.select || result.prismaKeys.array.select.key1 !== true) {
-//         throw new Error("Failed to parse prisma keys on array");
-//     }
-
-//     if (!result.prismaKeys.object.select || result.prismaKeys.object.select.ia !== true) {
-//         throw new Error("Failed to parse prisma keys on object");
-//     }
-
-//     if (result.prismaKeys.union !== true) {
-//         throw new Error("Failed to parse prisma keys on union");
-//     }
-
-//     if (!result.prismaKeys.unionObject.select || result.prismaKeys.unionObject.select.a !== true) {
-//         throw new Error("Failed to parse prisma keys on union object");
-//     }
-
-//     if (!result.prismaKeys.intersection.select || result.prismaKeys.intersection.select.a !== true) {
-//         throw new Error("Failed to parse prisma keys on intersection");
-//     }
-
-//     // #endregion
-
-//     console.log("> End test to parse Zod Schema");
-// };
-
 describe("Data Formatter (`formatObject` and `formatFormData`)", () => {
     type ResultTestObject = {
         uuid: string;
@@ -162,6 +53,8 @@ describe("Data Formatter (`formatObject` and `formatFormData`)", () => {
             };
         };
     };
+
+    const testFile = new File(["test"], "test.ts");
 
     const inputTestObject = {
         uuid: randomUUID(),
